c09d6c30668b19e615f0515eec7e59ef15931184
anhle1476 <anhle1476@users.noreply.github.com>
2026-08-30 18:44:15 +0700

    Allow all Vite development hosts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 5173,
+    allowedHosts: true,
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp'
